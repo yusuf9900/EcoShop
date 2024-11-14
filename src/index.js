64cfb64a558d012683cb324db2beb74216439a00
Youssou SALL <youssuf9900@gmail.com>
2024-11-14 22:39:16 +0000

    Il y a eu des erreurs parceque dans les pages App.js, index.js, App.css, index.css et index.html leurs code était écrit deux fois

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
@@ -10,21 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-=======
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> d1a1e9671da7abc31467b5a420a3656869abfa78
