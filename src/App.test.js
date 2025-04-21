9c8a9e29a7fadee0ba89b69a33f968976118c367
yusuf9900 <youssuf9900@gmail.com>
2025-04-21 12:56:20 +0000

    configuration de App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders EcoShop homepage title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/EcoShop : Consommez autrement, respectez la planète !/i);
+  expect(titleElement).toBeInTheDocument();
 });
