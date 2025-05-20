dbae3eb01baedcadc8bdca45c17645112ec5ca33
yusuf9900 <youssuf9900@gmail.com>
2025-05-20 16:59:00 +0000

    complement barre de recherche

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -57,10 +57,7 @@ function NavBar({ nombreProduits }) {  // La prop 'nombreProduits' est reçue po
             className="search-input"
           />
           <button type="submit" className="search-button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <circle cx="11" cy="11" r="8"></circle>
-              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-            </svg>
+            Rechercher
           </button>
         </form>
       </div>
